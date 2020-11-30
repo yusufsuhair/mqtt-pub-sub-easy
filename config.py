97837cc6989f5ab4e58440355794b728c2970bad
Mohd Suhair, Yusuf <Yusuf.MohdSuhair@sicpa.com>
2020-11-30 12:30:13 +0800

    Modified

---
+++ config.py
@@ -0,0 +1,3 @@
+broker = "broker.hivemq.com"
+port = 1883
+topic = "testyusuf1998"
--- subscriber.py
+++ subscriber.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 import mysql.connector
 from datetime import date
+import config
 
 # mydb = mysql.connector.connect(   # Connect into DB (OPTIONAL)
 #     host="localhost",
@@ -10,21 +11,8 @@ from datetime import date
 # )
 
 
-broker = "broker.hivemq.com"
-port = 1883
-topic = "testyusuf1998" # If you use global broker, it is best to make it unique
-
-
 def on_message(client, userdata, msg): # Create function for callback
-    payload = str(msg.payload)
-
-    # Removing "b" and "'' from message from the publisher.
-    # Idk, its auto generated ...
-    payload = payload.translate({ord("'"): None})
-    payload = payload.translate({ord("b"): None})
-    payload = payload.replace(' ', '')
-    payload.translate({ord("'"): None})
-
+    payload = str(msg.payload.decode("utf-8"))
     print(payload)
 
     # mycursor = mydb.cursor()   # Insert into DB (OPTIONAL)
@@ -36,6 +24,6 @@ def on_message(client, userdata, msg): # Create function for callback
 
 mqtt_client = mqtt.Client()  # MQTT Client Initialization
 mqtt_client.on_message = on_message  # OnMessage callback
-mqtt_client.connect(broker, port, 60)  # Connect to the broker
-mqtt_client.subscribe(topic)  # Subscribe to a topic.
+mqtt_client.connect(config.broker, config.port, 60)  # Connect to the broker
+mqtt_client.subscribe(config.topic)  # Subscribe to a topic.
 mqtt_client.loop_forever()  # Make it nonstop
